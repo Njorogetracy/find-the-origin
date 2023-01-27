b3ec6500372ab0d7b8a9057d4a4bf4adc07db1e4
Hope Tracy <hnjoroge64@gmail.com>
2023-01-27 18:16:17 +0000

    Add start quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Quiz questions
 
 let questions = [
+
     {
         question: 'What country has the longest total coastline (including mainland and offshore islands)?',
         answer: [
@@ -207,3 +208,66 @@ let questions = [
 
 
 //Getting elements from the Dom
+
+const openModal = document.getElementById("myModal");
+const startButton = document.getElementsByTagName("continue");
+let rules = document.getElementById("instructions");
+let quizLoad = document.getElementsByClassName('question-container');
+let answerButton1 = document.getElementById("answer1");
+let answerButton2 = document.getElementById("answer2");
+let answerButton3 = document.getElementById("answer3");
+let answerButton4 = document.getElementById("answer4");
+let scoreElement = document.getElementsByClassName("scoreArea");
+
+
+// Eventlisteners
+
+openModal.addEventListener('click', startGame)
+
+// when starting quiz
+
+let currentQuiz = 0
+let score = 0
+
+function startGame() {
+    console.log('started')
+    myModal.classList.add('active')
+
+}
+
+function startQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questions.innerText = currentQuizData.question
+    answer1.innerText = currentQuizData.answerButton1
+    answer2.innerText = currentQuizData.answerButton2
+    answer3.innerText = currentQuizData.answerButton3
+    answer4.innerText = currentQuizData.answerButton4
+}
+
+function deselectAnswers() {
+
+}
+
+function  checkAnswer() {
+
+}
+
+function increementCorrectAnswer() {
+
+}
+
+function increementWrongAnswer() {
+
+}
+
+function setNextQuestion() {
+
+}
+
+function endQuiz() {
+
+}
