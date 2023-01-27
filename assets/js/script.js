0ce6c4dbac0eeaf9b3f8ebf8b621471c8ceb34e3
Hope Tracy <hnjoroge64@gmail.com>
2023-01-27 00:38:58 +0000

    Add quiz questions and answers to js script

---
+++ assets/js/script.js
@@ -0,0 +1,209 @@
+// Quiz questions
+
+let questions = [
+    {
+        question: 'What country has the longest total coastline (including mainland and offshore islands)?',
+        answer: [
+            {text: 'Sweden'},
+            {text: 'Russia'},
+            {text: 'Canada', correct: 'true'},
+            {text: 'Norway'},
+        ]
+    },
+
+    {
+        question: 'Which of these countries is NOT in Africa?',
+        answer: [
+            {text: 'Guyana', correct: 'true'},
+            {text: 'Guinea'},
+            {text: 'Equatorial Guinea'},
+            {text: 'Sao Tome and Principe'},
+
+        ]
+    },
+
+    {
+        question: 'Which of these countries is the most densely populated?',
+        answer: [
+            {text: 'Hong Kong'},
+            {text: 'Monaco'},
+            {text: 'Singapore', correct: 'true'},
+            {text: 'Bahrain'},
+
+        ]
+    },
+
+    {
+        question: 'Caberra is the capital city of what country?',
+        answer: [
+            {text: 'New Zealand'},
+            {text: 'Australia', correct: 'true'},
+            {text: 'Scotland'},
+            {text: 'Wales'},
+
+        ]
+    },
+
+    {
+        question: 'Which is the 2nd most populous country?',
+        answer: [
+            {text: 'China'},
+            {text: 'USA'},
+            {text: 'India', correct: 'true'},
+            {text: 'Brazil'},
+        ]
+    },
+
+    {
+        question: 'Which country is the source of the White Nile',
+        answer: [
+            {text: 'Ethiopia'},
+            {text: 'Uganda', correct: 'true'},
+            {text: 'South Sudan'},
+            {text: 'Egypt'},
+        ]
+    },
+
+    {
+        question: 'Which country is famously known for Hieroglyphics?',
+        answer: [
+            {text: 'Greece'},
+            {text: 'Iran'},
+            {text: 'Egypt', correct: 'true'},
+            {text: 'Israel'},
+        ]
+    },
+
+    {
+        question: 'Which of the following countries does not qualify to be an enclave',
+        answer: [
+            {text: 'Vatican City'},
+            {text: 'Lesotho'},
+            {text: 'Swaziland', correct: 'true'},
+            {text: 'San Marino'},
+        ]
+    },
+
+    {
+        question: 'Which of the following European countries is not landlocked?',
+        answer: [
+            {text: 'Maldova'},
+            {text: 'Belarus'},
+            {text: 'Montenegro', correct: 'true'},
+            {text: 'North Macedonia'},
+        ]
+    },
+
+    {
+        question: 'Which country is home to the highest mountain in Africa',
+        answer: [
+            {text: 'Kenya'},
+            {text: 'Algeria'},
+            {text: 'South Africa'},
+            {text: 'Tanzania', correct: 'true'},
+        ]
+    },
+
+    {
+        question: 'Which country is the birthplace of ramen noodles?',
+        answers: [
+            {text: 'China', correct: 'true'},
+            {text: 'Japan'},
+            {text: 'Vietnam'},
+            {text: 'South Korea'},
+        ]
+    },
+
+    {
+        question: 'Which islands are not located in the Pacific Ocean?',
+        answer: [
+            {text: 'Fiji'},
+            {text: 'Ambergris Caye', correct: 'true'},
+            {text: 'Papua New Guinea'},
+            {text: 'Kiribati'},
+        ]
+    },
+
+    {
+        question: 'Which European country does not use the Euro as its currency?',
+        answer: [
+            {text: 'Norway', correct: 'true'},
+            {text: 'France'},
+            {text: 'Germany'},
+            {text: 'Austria'},
+        ]
+    },
+
+    {
+        question: 'Which country is home to the Amazon Forest?',
+        answer: [
+            {text: 'USA'},
+            {text: 'Brazil', correct: 'true'},
+            {text: 'Chile'},
+            {text: 'Colombia'},
+        ]
+    },
+
+    {
+        question: 'Which is not a Scandinavian country?',
+        answer: [
+            {text: 'Iceland'},
+            {text: 'Denmark'},
+            {text: 'Finland'},
+            {text: 'Netherlands', correct: 'true'},
+        ]
+    },
+
+    {
+        question: 'Which country is home to the Nobel Peace Prize?',
+        ansnwer: [
+            {text: 'Sweden'},
+            {text: 'Norway', correct: 'true'},
+            {text: 'Germany'},
+            {text: 'USA'},
+        ]
+    },
+
+    {
+        question: 'In which country is Arabica coffee not grown?',
+        answer: [
+            {text: 'Brazil'},
+            {text: 'Kenya'},
+            {text: 'Ethiopia'},
+            {text: 'Uganda', correct: 'true'},
+        ]
+    },
+
+    {
+        question: 'Which African country is closest to the Strait of Gilbraltar?',
+        answer: [
+            {text: 'Tunisia'},
+            {text: 'Morocco', correct: 'true'},
+            {text: 'Algeria'},
+            {text: 'Libya'},
+        ]
+    },
+
+    {
+        queston: 'Which country is home to the International Criminal Court?',
+        answer: [
+            {text: 'Netherlands', correct: 'true'},
+            {text: 'Germany'},
+            {text: 'Austria'},
+            {text: 'Hungary'}
+        ]
+    },
+
+    {
+        queston: 'What country is Mesopotamia, one of the first centers of civilization, today?',
+        answer: [
+            {text: 'Iran'},
+            {text: 'Syria'},
+            {text: 'Iraq', correct: 'true'},
+            {text: 'Israel'}
+        ]    
+    },
+]
+
+
+//Getting elements from the Dom
