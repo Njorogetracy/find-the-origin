a9e2dc232ed6243c20c868fd18592bdb1c91f383
Hope Tracy <hnjoroge64@gmail.com>
2023-01-31 20:59:09 +0000

    Add close rules function to the modal button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 //Getting elements from the Dom
 
 let introPage = document.getElementById("intro");
-let openModal = document.getElementById("button-rule")
+let ruleModal = document.getElementById("button-rule")
 let ruleBtn = document.getElementById("instructions")
-let closeModal = document.getElementById("close-btn")
+let closeModal = document.getElementById("close-rules")
 let rulesText = document.getElementsByClassName("modal-header")
 let startButton = document.getElementById("button-start")
 let quizLoad = document.getElementsByClassName('question-container');
@@ -15,70 +15,30 @@ let scoreElement = document.getElementsByClassName("scoreArea");
 
 
 // Modal with instructions
+ruleModal.addEventListener('click', function() {
+  ruleModal.style.display = 'none';
+  introPage.style.display = 'none';
+  startButton.style.display = 'none'
+  instructions.style.display = 'block';
+  showInstructions();
+  console.log('hey modal');
+});
 
-openModal.addEventListener('click', function() {
-    openModal.style.display = 'none';
-    introPage.style.display = 'hide';
-    instructions.style.display = 'block';
-    showInstructions();
-    console.log('hey modal');
-  });
-  
-  function showInstructions() {
-    instructions.style.display = 'block';
-    console.log('show rules');
-  };
+function showInstructions() {
+  instructions.style.display = 'block';
+  console.log('show rules');
+};
 
-  closeModal.addEventListener('click', function() {
-    closeModal.addEventListener = 'block';
-    instructions.style.display = 'none';
-  } )
+closeModal.addEventListener('click', function() {
+ closeModal.addEventListener = 'block';
+ instructions.style.display = 'block';
+ introPage.style.display = 'block';
+ closeRules();
+} )
 
-
-// when starting quiz
-
-let currentQuiz = 0
-let score = 0
-
-//function startGame() {
-  //  console.log('started')
-    //myModal.classList.add('active')
-
-//}
-
-//function startQuiz() {
-
-//    deselectAnswers()
-
-//    const currentQuizData = quizData[currentQuiz]
-
-//    questions.innerText = currentQuizData.question
-//    answer1.innerText = currentQuizData.answerButton1
-//    answer2.innerText = currentQuizData.answerButton2
-//    answer3.innerText = currentQuizData.answerButton3
-//    answer4.innerText = currentQuizData.answerButton4
-//}
-
-function deselectAnswers() {
-
-}
-
-function  checkAnswer() {
-
-}
-
-function increementCorrectAnswer() {
-
-}
-
-function increementWrongAnswer() {
-
-}
-
-function setNextQuestion() {
-
-}
-
-function endQuiz() {
-
-}
+function closeRules() {
+instructions.style.display = 'none';
+introPage.style.display = 'block';
+startButton.style.display = 'block';
+ruleModal.style.display = 'block';
+};
