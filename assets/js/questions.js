79d03e8fd8e7508f2077979a256c24866283ea42
Hope Tracy <hnjoroge64@gmail.com>
2023-02-03 00:41:58 +0000

    Add onclick function to start button in index.html

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,4 @@
-// Quiz questions
+// Quiz 
 
 let question = [
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,18 @@
 //Getting elements from the Dom
 
 let introPage = document.getElementById("intro");
-let myModal = document.getElementById("button-rule");
+let myModal = document.querySelector("#button-rule");
 let ruleBtn = document.getElementById("instructions");
-let closeModal = document.getElementById("close-rules");
+let closeModal = document.querySelector("#close-rules");
 let rulesText = document.getElementsByClassName("modal-header");
-let startButton = document.getElementById("start-game");
-let quizLoad = document.getElementsByClassName('question-container');
-let quizQuestion = document.getElementById('question');
+let startButton = document.querySelector("#button-start");
+let quizLoad = document.querySelector('.question-container');
+let quizQuestion = document.getElementById('questions');
 let answerButton1 = document.getElementById("answer1");
 let answerButton2 = document.getElementById("answer2");
 let answerButton3 = document.getElementById("answer3");
 let answerButton4 = document.getElementById("answer4");
 let scoreElement = document.getElementsByClassName("scoreArea");
-
-
-startButton.addEventListener('click', function() {
-    console.log("you clicked start")
-    startQuiz();
-    
-});
-
-function startQuiz() {
-  console.log('game started')
-
-  //deselectAnswers();
-
-  let currentQuizQuestion = question[currentQuiz]
-
-  quizQuestion.innerText = currentQuizQuestion.question
-  answerButton1.innerText = currentQuizQuestion.answer1
-  answerButton2.innerText = currentQuizQuestion.answer2
-  answerButton3.innerText = currentQuizQuestion.answer3
-  answerButton4.innerText = currentQuizQuestion.answer4
-}
 
 
 // open Modal with instructions and close modal
@@ -67,7 +46,31 @@ startButton.style.display = 'block';
 myModal.style.display = 'block';
 };
 
+// starting the game
 
-// starting the game 
+let shuffledQuestions, currentQuestionShow
+
+startButton.addEventListener("click", startQuiz)
+quizLoad.addEventListener("load", setQuestion)
+
+function startQuiz() {
+  console.log('game started')
+  startButton.classList.add("hide")
+}
+
+function setQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionShow])
+}
+
+function showQuestion(currentQuizQuestion) {
+  quizQuestion.innerText = currentQuizQuestion.question
+  answerButton1.innerText = currentQuizQuestion.answer1
+  answerButton2.innerText = currentQuizQuestion.answer2
+  answerButton3.innerText = currentQuizQuestion.answer3
+  answerButton4.innerText = currentQuizQuestion.answer4
+}
 
 
+
+
+  
