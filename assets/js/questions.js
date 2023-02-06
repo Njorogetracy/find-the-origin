b027b96945437eade92423a78accdc150dedc7cf
Hope Tracy <hnjoroge64@gmail.com>
2023-02-06 21:49:10 +0000

    Add score correct increement and questions loop

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,20 +4,20 @@ let question = [
     {
         question: 'What country has the longest total coastline (including mainland and offshore islands)?',
         answer: [
-            {text: 'Sweden', correct: false},
-            {text: 'Russia', correct: false},
-            {text: 'Canada', correct: true},
-            {text: 'Norway', correct: false},
+            {text: 'Sweden'},
+            {text: 'Russia'},
+            {text: 'Canada', correct: "true"},
+            {text: 'Norway'},
         ]
     },
   
     {
         question: 'Which of these countries is NOT in Africa?',
         answer: [
-            {text: 'Guyana', correct: true},
-            {text: 'Guinea', correct: false},
-            {text: 'Equatorial Guinea', correct: false},
-            {text: 'Sao Tome and Principe', correct: false},
+            {text: 'Guyana', correct: "true"},
+            {text: 'Guinea'},
+            {text: 'Equatorial Guinea'},
+            {text: 'Sao Tome and Principe'},
   
         ]
     },
@@ -25,10 +25,10 @@ let question = [
     {
         question: 'Which of these countries is the most densely populated?',
         answer: [
-            {text: 'Hong Kong', correct: false},
-            {text: 'Monaco', correct: false},
-            {text: 'Singapore', correct: true},
-            {text: 'Bahrain', correct: false},
+            {text: 'Hong Kong'},
+            {text: 'Monaco'},
+            {text: 'Singapore', correct: "true"},
+            {text: 'Bahrain'},
   
         ]
     },
@@ -36,10 +36,10 @@ let question = [
     {
         question: 'Canberra is the capital city of what country?',
         answer: [
-            {text: 'New Zealand', correct: false},
-            {text: 'Australia', correct: true},
-            {text: 'Scotland', correct: false},
-            {text: 'Wales', correct: false},
+            {text: 'New Zealand'},
+            {text: 'Australia', correct: "true"},
+            {text: 'Scotland'},
+            {text: 'Wales'},
   
         ]
     },
@@ -47,160 +47,160 @@ let question = [
     {
         question: 'Which is the 2nd most populous country?',
         answer: [
-            {text: 'China', correct: false},
-            {text: 'USA', correct: false},
-            {text: 'India', correct: true},
-            {text: 'Brazil', correct: false},
+            {text: 'China'},
+            {text: 'USA'},
+            {text: 'India', correct: "true"},
+            {text: 'Brazil'},
         ]
     },
   
     {
         question: 'Which country is the source of the White Nile?',
         answer: [
-            {text: 'Ethiopia', correct: false},
-            {text: 'Uganda', correct: true},
-            {text: 'South Sudan', correct: false},
-            {text: 'Egypt', correct: false},
+            {text: 'Ethiopia'},
+            {text: 'Uganda', correct: "true"},
+            {text: 'South Sudan'},
+            {text: 'Egypt'},
         ]
     },
   
     {
         question: 'Which country is famously known for Hieroglyphics?',
         answer: [
-            {text: 'Greece', correct: false},
-            {text: 'Iran' , correct: false},
-            {text: 'Egypt', correct: true},
-            {text: 'Israel', correct: false},
+            {text: 'Greece'},
+            {text: 'Iran'  },
+            {text: 'Egypt', correct: "true"},
+            {text: 'Israel'},
         ]
     },
   
     {
         question: 'Which of the following countries does not qualify to be an enclave?',
         answer: [
-            {text: 'Vatican City', correct: false},
-            {text: 'Lesotho', correct: false},
-            {text: 'Swaziland', correct: true},
-            {text: 'San Marino', correct: false},
+            {text: 'Vatican City'},
+            {text: 'Lesotho'},
+            {text: 'Swaziland', correct: "true"},
+            {text: 'San Marino'},
         ]
     },
   
     {
         question: 'Which of the following European countries is not landlocked?',
         answer: [
-            {text: 'Moldova', correct: false},
-            {text: 'Belarus', correct: false},
-            {text: 'Montenegro', correct: true},
-            {text: 'North Macedonia', correct: false},
+            {text: 'Moldova'},
+            {text: 'Belarus'},
+            {text: 'Montenegro', correct: "true"},
+            {text: 'North Macedonia'},
         ]
     },
   
     {
         question: 'Which country is home to the highest mountain in Africa?',
         answer: [
-            {text: 'Kenya', correct: false},
-            {text: 'Algeria', correct: false},
-            {text: 'South Africa', correct: false},
-            {text: 'Tanzania', correct: true},
+            {text: 'Kenya'},
+            {text: 'Algeria'},
+            {text: 'South Africa'},
+            {text: 'Tanzania', correct: "true"},
         ]
     },
   
     {
         question: 'Which country is the birthplace of ramen noodles?',
         answer: [
-            {text: 'China', correct: true},
-            {text: 'Japan', correct: false},
-            {text: 'Vietnam', correct: false},
-            {text: 'South Korea', correct: false},
+            {text: 'China', correct: "true"},
+            {text: 'Japan'},
+            {text: 'Vietnam'},
+            {text: 'South Korea'},
         ]
     },
   
     {
         question: 'Which islands are not located in the Pacific Ocean?',
         answer: [
-            {text: 'Fiji', correct: false},
-            {text: 'Ambergris Caye', correct: true},
-            {text: 'Papua New Guinea', correct: false},
-            {text: 'Kiribati', correct: false},
+            {text: 'Fiji'},
+            {text: 'Ambergris Caye', correct: "true"},
+            {text: 'Papua New Guinea'},
+            {text: 'Kiribati'},
         ]
     },
   
     {
         question: 'Which European country does not use the Euro as its currency?',
         answer: [
-            {text: 'Norway', correct: true},
-            {text: 'France', correct: false},
-            {text: 'Germany', correct: false},
-            {text: 'Austria', correct: false},
+            {text: 'Norway', correct: "true"},
+            {text: 'France'},
+            {text: 'Germany'},
+            {text: 'Austria'},
         ]
     },
   
     {
         question: 'Which country is home to the Amazon Forest?',
         answer: [
-            {text: 'USA', correct: false},
-            {text: 'Brazil', correct: true},
-            {text: 'Chile', correct: false},
-            {text: 'Colombia', correct: false},
+            {text: 'USA'},
+            {text: 'Brazil', correct: "true"},
+            {text: 'Chile'},
+            {text: 'Colombia'},
         ]
     },
   
     {
         question: 'Which of the following is not a Scandinavian country?',
         answer: [
-            {text: 'Iceland', correct: false},
-            {text: 'Denmark', correct: false},
-            {text: 'Finland', correct: false},
-            {text: 'Netherlands', correct: true},
+            {text: 'Iceland'},
+            {text: 'Denmark'},
+            {text: 'Finland'},
+            {text: 'Netherlands', correct: "true"},
         ]
     },
   
     {
         question: 'Which country is home to the Nobel Peace Prize?',
-        ansnwer: [
-            {text: 'Sweden', correct: false},
-            {text: 'Norway', correct: true},
-            {text: 'Germany', correct: false},
-            {text: 'USA', correct: false},
+        answer: [
+            {text: 'Sweden'},
+            {text: 'Norway', correct: "true"},
+            {text: 'Germany'},
+            {text: 'USA'},
         ]
     },
   
     {
         question: 'In which country is Arabica coffee NOT grown?',
         answer: [
-            {text: 'Brazil', correct: false},
-            {text: 'Kenya', correct: false},
-            {text: 'Ethiopia', correct: false},
-            {text: 'Uganda', correct: true},
+            {text: 'Brazil'},
+            {text: 'Kenya'},
+            {text: 'Ethiopia'},
+            {text: 'Uganda', correct: "true"},
         ]
     },
   
     {
         question: 'Which African country is closest to the Strait of Gibraltar?',
         answer: [
-            {text: 'Tunisia', correct: false},
-            {text: 'Morocco', correct: true},
-            {text: 'Algeria', correct: false},
-            {text: 'Libya', correct: false},
+            {text: 'Tunisia'},
+            {text: 'Morocco', correct: "true"},
+            {text: 'Algeria'},
+            {text: 'Libya'},
         ]
     },
   
     {
         question: 'Which country is home to the International Criminal Court?',
         answer: [
-            {text: 'Netherlands', correct: true},
-            {text: 'Germany', correct: false},
-            {text: 'Austria', correct: false},
-            {text: 'Hungary', correct: false}
+            {text: 'Netherlands', correct: "true"},
+            {text: 'Germany'},
+            {text: 'Austria'},
+            {text: 'Hungary'}
         ]
     },
   
     {
         question: 'Which modern-day country is home to Mesopotamia, one of the first centres of civilization?',
         answer: [
-            {text: 'Iran', correct: false},
-            {text: 'Syria', correct: false},
-            {text: 'Iraq', correct: true},
-            {text: 'Israel', correct: false}
+            {text: 'Iran'},
+            {text: 'Syria'},
+            {text: 'Iraq', correct: "true"},
+            {text: 'Israel'}
         ]    
     },
   ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -36,20 +36,37 @@
     myModal.style.display = 'block';
   }
 
-
-// start quiz
-
 let startButton = document.getElementById("button-start");
 
 window.onload = (event) => {
   startButton.addEventListener("click", startQuiz);
 };
 
+function shuffleNum(){
+  let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+  let randomNumber = [];
+  for (let i = 0; i<=9; i++) {
+    let randomIndex = Math.floor(Math.random()* numbers.length);
+    randomNumber.push(numbers[randomIndex]);
+    numbers.splice(randomIndex, 1);
+  }
+return randomNumber
+ }
+let shuffled = shuffleNum()
+console.log(shuffled)
 
 // start quiz
 let e = 0;
+let score = 0;
 function startQuiz(event) {
+  let f = shuffled[e]
   let gameStart = document.getElementById("game");
+  if (e === 10) {
+    gameStart.innerHTML = `
+    <h1> Game Over </h1>
+    <h2> Your score is ${score}</h2>
+    `
+  } 
   startButton.classList.add('hide');
   gameStart.innerHTML = `
     <div class="text-center text-light score-area">
@@ -58,13 +75,13 @@ function startQuiz(event) {
             Score
         </strong>
     </p>
-    <h1>0</h1>
+    <h1>${score}</h1>
   </div>
   <div class="game-space question-container container-fluid col-6 mt-4 text-center">
     <br>
     <div class="quiz-header">
         <h2 class="text-light question" id="questions">
-            ${question[e].question}
+            ${question[f].question}
         </h2>
         <br>
         <div class="btn-group-vertical answer">
@@ -72,32 +89,40 @@ function startQuiz(event) {
     </div>
   </div>`;
   for (let i=0; i<4; i++) {
-    let answerEd = question[e].answer[i].text;
+    let answerEd = question[f].answer[i].text;
     let answerButton = document.querySelector('.answer');
-    answerButton.innerHTML+=`
-    <div class="btn btn-primary btn-lg answer"id="answer">${answerEd}</div>
+    if (question[f].answer[i].correct === "true") {
+      answerButton.innerHTML+=`
+    <button class="btn btn-lg text-light btn-primary correct">${answerEd}</button>
      <br>`;
+     console.log(answerEd)
+    } else {
+    answerButton.innerHTML+=`
+    <button class="btn btn-lg text-light btn-primary">${answerEd}</button>
+     <br>`;
+     console.log(answerEd) }
    };
-
-   let answers = document.querySelectorAll('.answer');
-     for (let j=0; j<9; j++) {
+   let answers = document.querySelectorAll('.btn');
+     for (let j=0; j<4; j++) {
       answers[j].addEventListener("click", answerSelect);
-      e++;
    };
-  
+   e++;
 }
 
 function answerSelect(event) {
-  let correctAnswer = event.target.correct;
-  if (correctAnswer) {
-    console.log("correct");
-    event.target.style.backgroundColor = "green";
- } else {
-  console.log("wrong");
-   event.target.style.backgroundColor = "red"
-   };
-   setTimeout(startQuiz, 1000);
-   console.log(correctAnswer);
+  let selected = event.target;
+  if (selected.classList.contains("correct")) {
+    console.log("Correct!");
+    score+=1;
+    selected.style.backgroundColor = "green";
+  } else {
+    console.log("Wrong");
+    selected.style.backgroundColor = "red";
+  }
+  setTimeout(startQuiz, 1000);
 }
+
+
+
 
 
